20d8f395ce688977c7bc9bd592cc617465c4eab4
anass17 <anassboutaib2018@gmail.com>
2024-12-09 02:07:51 +0100

    Added Tailwind

---
+++ inc/db-connect.php
@@ -0,0 +1,38 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "root123";
+$db = "cars_management";
+
+$conn = new mysqli($server, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// To insert
+
+// $name = "John Doe";
+// $email = "john.doe@example.com";
+// $sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+
+// if ($conn->query($sql) === TRUE) {
+//     echo "New record created successfully";
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
+// To Select
+// $sql = "SELECT * FROM clients";
+// $result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+//     while($row = $result->fetch_assoc()) {
+//         echo '<pre>';
+//         print_r($row);
+//         echo '</pre>';
+//     }
+// } else {
+//     echo "0 results";
+// }
--- index.php
+++ index.php
@@ -1,13 +1,19 @@
+<?php
+    require "inc/db-connect.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
     
+
     <script src="script.js"></script>
 </body>
 </html>
