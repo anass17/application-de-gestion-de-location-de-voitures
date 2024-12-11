3521519b746bf98977893183833e176aa393d434
anass17 <anassboutaib2018@gmail.com>
2024-12-11 16:30:41 +0100

    Added delete PHP functionality for clients

--- assets/pages/delete.php
+++ assets/pages/delete.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
 
     require "../../inc/db-connect.php";
 
@@ -41,6 +42,37 @@
             $stmt -> bind_param("i", $contractId);
 
             if ($stmt -> execute()) {
+                $_SESSION['msg'] = "The contract was successfully deleted";
+                $_SESSION['status'] = "success";
+                header('Location: ../../index.php?page=' . $page . '&view=' . $view);
+            } else {
+                set_error("Could not process your request", $page, $view);
+                exit();
+            }
+        } else if ($page == "clients") {
+            // Get values
+
+            $clientId = isset($_GET["id"]) ? $_GET["id"] : "";
+
+            // Validate values
+
+            if (preg_match('/^[1-9][0-9]*$/', $clientId) == 0) {
+                set_error("The client is invalid", $page, $view);
+                exit();
+            }
+
+            // Delete the row
+
+            $stmt = $conn -> prepare("DELETE FROM `clients` WHERE NumClient = ?");
+
+            if ($stmt === false) {
+                set_error("Could not process your request", $page, $view);
+                exit();
+            }
+
+            $stmt -> bind_param("i", $clientId);
+
+            if ($stmt -> execute()) {
                 $_SESSION['msg'] = "The client was successfully deleted";
                 $_SESSION['status'] = "success";
                 header('Location: ../../index.php?page=' . $page . '&view=' . $view);
@@ -48,6 +80,9 @@
                 set_error("Could not process your request", $page, $view);
                 exit();
             }
+        } else {
+            set_error("Could not process your request", "clients", "cards");
+            exit();
         }
     }
 
